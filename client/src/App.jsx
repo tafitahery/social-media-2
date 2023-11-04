0ff47033d6013fc3049c99d2a007ba73bfa229e7
Tafita Hery <tafitahery@gmail.com>
2023-11-04 18:15:49 +0300

    React Register Page Design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
-import Login from './pages/login/Login';
+// import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 function App() {
   return (
     <div>
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </div>
   );
 }
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,5 +1,30 @@
 import './register.scss';
 
 export default function Register() {
-  return <div className="register">Register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Lebo Social.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam
+            quasi repudiandae ad laudantium temporibus obcaecati porro
+            reprehenderit vel.
+          </p>
+          <span>Do you have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
