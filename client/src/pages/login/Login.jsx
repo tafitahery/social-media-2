7b53883c08b2dd7dd8763b2d82c7dec54ef30019
Tafita Hery <tafitahery@gmail.com>
2023-11-04 17:57:29 +0300

    React Login Page Design

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,5 +1,28 @@
 import './login.scss';
 
 export default function Login() {
-  return <div className="login">Login</div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. At aperiam
+            quasi repudiandae ad laudantium temporibus obcaecati porro
+            reprehenderit vel.
+          </p>
+          <span>Don't have an account?</span>
+          <button>Register</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
