0a45bfe02d6736931030e3a172106c38014d05e8
Tafita Hery <tafitahery@gmail.com>
2023-11-07 18:34:52 +0300

    React Router Dom Protected Route (Auth)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
-import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+  Navigate,
+  Outlet,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
 
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
@@ -9,6 +14,8 @@ import Leftbar from './components/leftbar/Leftbar';
 import Rightbar from './components/rightbar/Rightbar';
 
 function App() {
+  const currentUser = true;
+
   const Layout = () => {
     return (
       <div>
@@ -22,10 +29,22 @@ function App() {
     );
   };
 
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       children: [
         {
           path: '/',
